9705e0f49d25221c02c681787b4afed2ee624f3d
didlain123 <malikasorukova7@gmail.com>
2024-12-22 20:12:26 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const container = document.querySelector(".container"),
+percent = document.querySelector("#percent");
+
+
+let perVal = 0;
+
+let increament = setInterval(() => {
+    perVal++;
+    percent.textContent = `${perVal}%`;
+
+    if(perVal ==100){
+        clearInterval(increament);
+        container.classList.remove("active");
+    }
+    console.log(perVal);
+},100)
